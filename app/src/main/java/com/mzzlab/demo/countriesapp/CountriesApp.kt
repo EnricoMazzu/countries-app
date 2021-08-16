2e2c4796c49ed2c5cd439dc55aee473b39b84ef2
Enrico <enrico.mazzucchelli@vipera.com>
2021-08-16 17:15:07 +0200

    init logger

--- app/src/main/java/com/mzzlab/demo/countriesapp/CountriesApp.kt
+++ app/src/main/java/com/mzzlab/demo/countriesapp/CountriesApp.kt
@@ -2,7 +2,23 @@ package com.mzzlab.demo.countriesapp
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class CountriesApp: Application() {
+
+
+    override fun onCreate() {
+        super.onCreate();
+        initLogger();
+        Timber.i("CountriesApp init done")
+    }
+
+    private fun initLogger() {
+        if(BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+
 }
